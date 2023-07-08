bba114ceb0ab21d2df9f56425b1ad8d39726a37f
Franklin <71532004+fran1014@users.noreply.github.com>
2023-07-08 17:32:36 +0200

    Adding footer structure

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,37 @@
+import { footerLinks } from "@/constants";
+import Image from "next/image";
 import React from "react";
 
+const FooterColumn = () => (
+  <div className="footer_column">
+    <h4 className="font-semibold">Tittle</h4>
+    <ul className="flex flex-col gap-2 font-normal">Links</ul>
+  </div>
+);
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex items-start flex-col">
+          <Image
+            src="/logo-purple.svg"
+            width={115}
+            height={38}
+            alt="Flexibble"
+          />
+
+          <p className="text-start text-sm font-normal mt-5 max-w-xs">
+            Flexible is the world´s leading community for creatives to share,
+            grow and get hired fast.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn title={footerLinks} />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
