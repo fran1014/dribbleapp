4e46bb271acc0f33c080e5743abe6bc7faf18193
Franklin <71532004+fran1014@users.noreply.github.com>
2023-07-08 19:40:31 +0200

    loging and authorization set up

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
 
 const AuthProviders = () => {
+  const [providers, setproviders] = useState<Providers | null>(null);
   return <div>AuthProviders</div>;
 };
 
