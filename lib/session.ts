1d25cb79dc2c4a7405a5a72ba6aafef0fc3748a2
Franklin <71532004+fran1014@users.noreply.github.com>
2023-07-09 23:10:18 +0200

    configuring tokens

--- lib/session.ts
+++ lib/session.ts
@@ -7,6 +7,7 @@ import { JWT } from "next-auth/jwt";
 import { signIn } from "next-auth/react";
 import { SessionInterface, UserProfile } from "@/common.types";
 import { getUser } from "./actions";
+import grafbaseConfig from "@/grafbase/grafbase.config";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -15,10 +16,26 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  //jwt: {
-  // encode: ({ secret, token }) => {},
-  //decode: async ({ secret, token }) => {},
-  //},
+  jwt: {
+    encode: ({ secret, token }) => {
+      const encodedToken = jsonwebtoken.sign(
+        {
+          ...token,
+          iss: "grafbase",
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        },
+        secret
+      );
+
+      return encodedToken;
+    },
+
+    decode: async ({ secret, token }) => {
+      const decodeToken = jsonwebtoken.verify(token!, secret) as JWT;
+
+      return decodeToken;
+    },
+  },
   theme: {
     colorScheme: "light",
     logo: "/logo.png",
@@ -34,9 +51,11 @@ export const authOptions: NextAuthOptions = {
           ...session,
           user: { ...session.user, ...data?.user },
         };
-      } catch (error) {}
-
-      return session;
+        return newSession;
+      } catch (error) {
+        console.log("Error retrieving user data", error);
+        return session;
+      }
     },
   },
   async signIn({ user }: { user: AdapterUser | User }) {
